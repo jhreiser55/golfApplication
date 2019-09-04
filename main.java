2cedb41ffba4d96570a8fa5379e59b1273f8bcd5
Jordan Reiser <jordanreiser1255@gmail.com>
2019-09-03 21:16:40 -0400

    More done with the file saving aspect of the app and introduction to commands.

--- main.java
+++ main.java
@@ -13,7 +13,8 @@ public class main{
 			if(args[0].endsWith(/*need to figure out file sav*/)){
 				state.restore(args[0]);
 				System.out.println("\nWelcome back " + state.getName();
-				//Finish copying from interpreter.java line 41		
+				//Finish copying from interpreter.java line 41
+				//Go finish GameState before doing this.		
 
 	}
 }
